31ad4e57d056442f7be6a1bccec77b25140721a9
hzokbe <hzokbe@gmail.com>
2025-07-05 20:42:24 -0300

    feat(repository): add MovieRepository interface

---
+++ src/main/java/com/hzokbe/eigakan/repository/movie/MovieRepository.java
@@ -0,0 +1,10 @@
+package com.hzokbe.eigakan.repository.movie;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hzokbe.eigakan.model.movie.Movie;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, String> {
+}
