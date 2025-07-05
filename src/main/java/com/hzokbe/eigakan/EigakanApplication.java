692ee10819df7bbdb72d466d604104dd7b3f7b0a
hzokbe <hzokbe@gmail.com>
2025-07-05 20:40:02 -0300

    chore: initial setup

---
+++ src/main/java/com/hzokbe/eigakan/EigakanApplication.java
@@ -0,0 +1,11 @@
+package com.hzokbe.eigakan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EigakanApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EigakanApplication.class, args);
+	}
+}
