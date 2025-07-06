b21fde8d113201f6437bebf43b2c0ba31480c66a
hzokbe <hzokbe@gmail.com>
2025-07-06 16:29:04 -0300

    feat(model): update MovieResponse class

--- src/main/java/com/hzokbe/eigakan/model/movie/response/MovieResponse.java
+++ src/main/java/com/hzokbe/eigakan/model/movie/response/MovieResponse.java
@@ -1,17 +1,23 @@
 package com.hzokbe.eigakan.model.movie.response;
 
+import com.hzokbe.eigakan.model.genre.Genre;
+
 public class MovieResponse {
     private String id;
 
     private String title;
 
+    private Genre genre;
+
     public MovieResponse() {
     }
 
-    public MovieResponse(String id, String title) {
+    public MovieResponse(String id, String title, Genre genre) {
         this.id = id;
         
         this.title = title;
+
+        this.genre = genre;
     }
 
     public String getId() {
@@ -28,5 +34,13 @@ public class MovieResponse {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
     }
 }
