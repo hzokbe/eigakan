cd56b9a253f9198065532645472bea83ff53311f
hzokbe <hzokbe@gmail.com>
2025-07-06 08:36:16 -0300

    feat(model): add MovieRequest class

---
+++ src/main/java/com/hzokbe/eigakan/model/movie/request/MovieRequest.java
@@ -0,0 +1,13 @@
+package com.hzokbe.eigakan.model.movie.request;
+
+public class MovieRequest {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
