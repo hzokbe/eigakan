03a419f6e6f7e1b972aec784af281ea3727c46c2
hzokbe <hzokbe@gmail.com>
2025-07-06 08:44:07 -0300

    feat(controller): add save method

--- src/main/java/com/hzokbe/eigakan/controller/movie/MovieController.java
+++ src/main/java/com/hzokbe/eigakan/controller/movie/MovieController.java
@@ -1,8 +1,14 @@
 package com.hzokbe.eigakan.controller.movie;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hzokbe.eigakan.model.movie.request.MovieRequest;
+import com.hzokbe.eigakan.model.movie.response.MovieResponse;
 import com.hzokbe.eigakan.service.movie.MovieService;
 
 @RestController
@@ -12,5 +18,10 @@ public class MovieController {
 
     public MovieController(MovieService service) {
         this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<MovieResponse> save(@RequestBody MovieRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(request));
     }
 }
