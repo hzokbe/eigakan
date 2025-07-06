79efbd624cbee6d190d9234033f571d9943660e9
hzokbe <hzokbe@gmail.com>
2025-07-06 08:37:12 -0300

    feat(model): add MovieResponse class

---
+++ src/main/java/com/hzokbe/eigakan/model/movie/response/MovieResponse.java
@@ -0,0 +1,29 @@
+package com.hzokbe.eigakan.model.movie.response;
+
+public class MovieResponse {
+    private String id;
+
+    private String title;
+
+    public MovieResponse(String id, String title) {
+        this.id = id;
+        
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
