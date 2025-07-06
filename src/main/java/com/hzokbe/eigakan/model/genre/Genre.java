d0a360af97cfbba21906fbcd95d46842a1b70c35
hzokbe <hzokbe@gmail.com>
2025-07-06 16:27:25 -0300

    feat(model): add Genre enum

---
+++ src/main/java/com/hzokbe/eigakan/model/genre/Genre.java
@@ -0,0 +1,24 @@
+package com.hzokbe.eigakan.model.genre;
+
+public enum Genre {
+    ACTION,
+    ADVENTURE,
+    ANIMATION,
+    COMEDY,
+    CRIME,
+    DOCUMENTARY,
+    DRAMA,
+    FANTASY,
+    HISTORICAL,
+    HORROR,
+    MUSICAL,
+    MYSTERY,
+    ROMANCE,
+    SCIENCE_FICTION,
+    THRILLER,
+    WESTERN,
+    BIOGRAPHY,
+    FAMILY,
+    WAR,
+    SPORT
+}
