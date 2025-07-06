c4507faeaa0d070c42f6a7572d611a2987f671ef
hzokbe <hzokbe@gmail.com>
2025-07-06 16:28:34 -0300

    feat(model): update MovieRequest class

--- src/main/java/com/hzokbe/eigakan/model/movie/request/MovieRequest.java
+++ src/main/java/com/hzokbe/eigakan/model/movie/request/MovieRequest.java
@@ -1,7 +1,11 @@
 package com.hzokbe.eigakan.model.movie.request;
 
+import com.hzokbe.eigakan.model.genre.Genre;
+
 public class MovieRequest {
     private String title;
+
+    private Genre genre;
 
     public String getTitle() {
         return title;
@@ -9,5 +13,13 @@ public class MovieRequest {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
     }
 }
