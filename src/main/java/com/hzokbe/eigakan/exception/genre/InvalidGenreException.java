93ae40778a63c9ec588c3d15e82cb7a5850cd769
hzokbe <hzokbe@gmail.com>
2025-07-06 16:30:17 -0300

    feat(exception): add InvalidGenreException class

---
+++ src/main/java/com/hzokbe/eigakan/exception/genre/InvalidGenreException.java
@@ -0,0 +1,7 @@
+package com.hzokbe.eigakan.exception.genre;
+
+public class InvalidGenreException extends RuntimeException {
+    public InvalidGenreException(String message) {
+        super(message);
+    }
+}
