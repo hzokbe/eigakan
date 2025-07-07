f26ddf8f728772416ad5bfb41b1b24c4e933de52
hzokbe <hzokbe@gmail.com>
2025-07-07 19:51:59 -0300

    feat(model): add PersonRequest class

---
+++ src/main/java/com/hzokbe/eigakan/model/person/request/PersonRequest.java
@@ -0,0 +1,23 @@
+package com.hzokbe.eigakan.model.person.request;
+
+public class PersonRequest {
+    private String name;
+
+    private String lastName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
