0197025bad8ed9e3c8bfa50d1da4e0f0ddced5ad
hzokbe <hzokbe@gmail.com>
2025-07-07 19:52:48 -0300

    feat(exception): add InvalidPersonLastNameException class

---
+++ src/main/java/com/hzokbe/eigakan/exception/person/InvalidPersonLastNameException.java
@@ -0,0 +1,7 @@
+package com.hzokbe.eigakan.exception.person;
+
+public class InvalidPersonLastNameException extends RuntimeException {
+    public InvalidPersonLastNameException(String message) {
+        super(message);
+    }
+}
