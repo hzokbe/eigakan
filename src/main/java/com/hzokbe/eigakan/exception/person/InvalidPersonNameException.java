d35bad38703fec6332836034563cbedb3bf025c5
hzokbe <hzokbe@gmail.com>
2025-07-07 19:52:37 -0300

    feat(exception): add InvalidPersonNameException class

---
+++ src/main/java/com/hzokbe/eigakan/exception/person/InvalidPersonNameException.java
@@ -0,0 +1,7 @@
+package com.hzokbe.eigakan.exception.person;
+
+public class InvalidPersonNameException extends RuntimeException {
+    public InvalidPersonNameException(String message) {
+        super(message);
+    }
+}
