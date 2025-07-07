56113f37cb1f12627c62de1184064f253e74ac40
hzokbe <hzokbe@gmail.com>
2025-07-07 19:54:08 -0300

    feat(service): add save method

--- src/main/java/com/hzokbe/eigakan/service/person/PersonService.java
+++ src/main/java/com/hzokbe/eigakan/service/person/PersonService.java
@@ -2,6 +2,12 @@ package com.hzokbe.eigakan.service.person;
 
 import org.springframework.stereotype.Service;
 
+import com.hzokbe.eigakan.exception.person.AlreadyRegisteredPersonException;
+import com.hzokbe.eigakan.exception.person.InvalidPersonLastNameException;
+import com.hzokbe.eigakan.exception.person.InvalidPersonNameException;
+import com.hzokbe.eigakan.model.person.Person;
+import com.hzokbe.eigakan.model.person.request.PersonRequest;
+import com.hzokbe.eigakan.model.person.response.PersonResponse;
 import com.hzokbe.eigakan.repository.person.PersonRepository;
 
 @Service
@@ -10,5 +16,41 @@ public class PersonService {
 
     public PersonService(PersonRepository repository) {
         this.repository = repository;
+    }
+
+    public PersonResponse save(PersonRequest request) {
+        var name = request.getName();
+
+        if (name == null) {
+            throw new InvalidPersonNameException("name cannot be null");
+        }
+
+        if (name.isBlank()) {
+            throw new InvalidPersonNameException("name cannot be blank");
+        }
+
+        var lastName = request.getLastName();
+
+        if (lastName == null) {
+            throw new InvalidPersonLastNameException("last name cannot be null");
+        }
+
+        if (lastName.isBlank()) {
+            throw new InvalidPersonLastNameException("last name cannot be blank");
+        }
+
+        name = name.trim();
+
+        lastName = lastName.trim();
+
+        if (repository.existsByNameAndLastName(name, lastName)) {
+            throw new AlreadyRegisteredPersonException("person already registered");
+        }
+
+        var person = new Person(name, lastName);
+
+        repository.save(person);
+
+        return new PersonResponse(person.getId(), name, lastName);
     }
 }
