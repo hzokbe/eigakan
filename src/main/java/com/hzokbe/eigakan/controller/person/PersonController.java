ae7aac0e1134c171ca61c453c88c1258c5c774c2
hzokbe <hzokbe@gmail.com>
2025-07-09 19:47:29 -0300

    feat(controller): add findById method

--- src/main/java/com/hzokbe/eigakan/controller/person/PersonController.java
+++ src/main/java/com/hzokbe/eigakan/controller/person/PersonController.java
@@ -1,10 +1,12 @@
 package com.hzokbe.eigakan.controller.person;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +33,10 @@ public class PersonController {
     @GetMapping
     public ResponseEntity<List<PersonResponse>> findAll() {
         return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PersonResponse> findById(@PathVariable UUID id) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.findById(id.toString()));
     }
 }
