dd502f425b1d5dbd7f1f5cf727a679694c36369d
hzokbe <hzokbe@gmail.com>
2025-07-14 14:26:46 -0300

    feat(configuration): add getPasswordEncoder method

--- src/main/java/com/hzokbe/eigakan/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/hzokbe/eigakan/configuration/security/SecurityConfiguration.java
@@ -1,9 +1,16 @@
 package com.hzokbe.eigakan.configuration.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new Argon2PasswordEncoder(16, 32, 1, 1 << 14, 2);
+    }
 }
