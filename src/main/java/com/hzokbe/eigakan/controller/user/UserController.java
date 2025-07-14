0ad408773cf18e876474407be75bbdabea6bbdc9
hzokbe <hzokbe@gmail.com>
2025-07-14 14:56:29 -0300

    feat(configuration): add getSecurityFilterChain method

--- src/main/java/com/hzokbe/eigakan/controller/user/UserController.java
+++ src/main/java/com/hzokbe/eigakan/controller/user/UserController.java
@@ -1,7 +1,11 @@
 package com.hzokbe.eigakan.controller.user;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +25,13 @@ public class UserController {
     @PostMapping("/sign-up")
     public ResponseEntity<UserResponse> signUp(@RequestBody UserRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.signUp(request));
+    }
+
+    @Bean
+    public SecurityFilterChain getSecurityFilterChain(HttpSecurity security) throws Exception {
+        return
+            security
+            .csrf(AbstractHttpConfigurer::disable)
+            .build();
     }
 }
