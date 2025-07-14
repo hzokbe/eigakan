96725ca54d2617431e8f0b94d8e6dd0e2edf0b0c
Lucas Fernandes <hzokbe@gmail.com>
2025-07-14 15:50:20 +0000

    feat(repository): add UserRepository interface

---
+++ src/main/java/com/hzokbe/eigakan/repository/user/UserRepository.java
@@ -0,0 +1,10 @@
+package com.hzokbe.eigakan.repository.user;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hzokbe.eigakan.model.user.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
