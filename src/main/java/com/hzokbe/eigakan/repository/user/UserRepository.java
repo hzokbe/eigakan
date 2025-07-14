46b773cd9d5c8ea0205db04f0fe592119ff34017
hzokbe <hzokbe@gmail.com>
2025-07-14 14:20:41 -0300

    feat(repository): add findByUsername method

--- src/main/java/com/hzokbe/eigakan/repository/user/UserRepository.java
+++ src/main/java/com/hzokbe/eigakan/repository/user/UserRepository.java
@@ -1,5 +1,7 @@
 package com.hzokbe.eigakan.repository.user;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.hzokbe.eigakan.model.user.User;
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
     boolean existsByUsername(String username);
+
+    Optional<User> findByUsername(String username);
 }
