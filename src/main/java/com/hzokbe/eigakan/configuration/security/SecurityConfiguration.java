7467e2634caa712b1160068956bb26d9a259c147
Lucas Fernandes <hzokbe@gmail.com>
2025-07-16 12:07:08 +0000

    feat(configuration): add rsaPublicKey attribute to SecurityConfiguration class

--- src/main/java/com/hzokbe/eigakan/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/hzokbe/eigakan/configuration/security/SecurityConfiguration.java
@@ -1,5 +1,8 @@
 package com.hzokbe.eigakan.configuration.security;
 
+import java.security.interfaces.RSAPublicKey;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,6 +12,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    @Value("${rsa.key.public}")
+    private RSAPublicKey rsaPublicKey;
+
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new Argon2PasswordEncoder(16, 32, 1, 1 << 14, 2);
