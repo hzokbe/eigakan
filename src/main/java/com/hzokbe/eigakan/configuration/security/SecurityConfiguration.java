9cd83f5df06b8f47439f9423572ecb41b1aba181
hzokbe <hzokbe@gmail.com>
2025-07-21 19:17:35 -0300

    feat(configuration): add getJwtEncoder method

--- src/main/java/com/hzokbe/eigakan/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/hzokbe/eigakan/configuration/security/SecurityConfiguration.java
@@ -3,12 +3,18 @@ package com.hzokbe.eigakan.configuration.security;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -22,5 +28,14 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new Argon2PasswordEncoder(16, 32, 1, 1 << 14, 2);
+    }
+
+    @Bean
+    public JwtEncoder getJwtEncoder() {
+        var jwk = new RSAKey.Builder(rsaPublicKey).privateKey(rsaPrivateKey).build();
+
+        var jwkSet = new ImmutableJWKSet<SecurityContext>(new JWKSet(jwk));
+
+        return new NimbusJwtEncoder(jwkSet);
     }
 }
