2dd10069b04820fba84ac47a54bc8725aa759355
hzokbe <hzokbe@gmail.com>
2025-07-30 16:36:15 -0300

    feat(service): implement UserDetailsService interface

--- src/main/java/com/hzokbe/eigakan/service/user/custom/CustomUserDetailsService.java
+++ src/main/java/com/hzokbe/eigakan/service/user/custom/CustomUserDetailsService.java
@@ -1,13 +1,35 @@
 package com.hzokbe.eigakan.service.user.custom;
 
 import com.hzokbe.eigakan.repository.user.UserRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CustomUserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository repository;
 
     public CustomUserDetailsService(UserRepository repository) {
         this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var optionalUser = repository.findByUsername(username);
+
+        if (optionalUser.isEmpty()) {
+            throw new UsernameNotFoundException("username not found");
+        }
+
+        var user = optionalUser.get();
+
+        return User
+                .builder()
+                .username(username)
+                .password(user.getPasswordHash())
+                .roles(user.getRole().name())
+                .build();
     }
 }
