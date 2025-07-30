1b4700c66d8e67cb1a796c354935d00ba40fd0ec
hzokbe <hzokbe@gmail.com>
2025-07-30 16:32:16 -0300

    feat(service): add UserRepository dependency injection

--- src/main/java/com/hzokbe/eigakan/service/user/custom/CustomUserDetailsService.java
+++ src/main/java/com/hzokbe/eigakan/service/user/custom/CustomUserDetailsService.java
@@ -1,7 +1,13 @@
 package com.hzokbe.eigakan.service.user.custom;
 
+import com.hzokbe.eigakan.repository.user.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomUserDetailsService {
+    private final UserRepository repository;
+
+    public CustomUserDetailsService(UserRepository repository) {
+        this.repository = repository;
+    }
 }
