63eb4e3e7336db32f8e5495c3f95cbea25f3fda8
hzokbe <hzokbe@gmail.com>
2025-08-03 17:44:09 -0300

    feat(configuration): update CORS configuration

--- src/main/java/com/hzokbe/eigakan/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/hzokbe/eigakan/configuration/security/SecurityConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -86,6 +87,7 @@ public class SecurityConfiguration {
                         .httpBasic(c -> c.authenticationEntryPoint((req, res, ex) -> {
                             res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
                         }))
+                        .cors(Customizer.withDefaults())
                         .oauth2ResourceServer(
                                 configurer -> configurer.jwt(
                                         converter -> converter.jwtAuthenticationConverter(getJwtAuthenticationConverter())
