0180e536c0cc551258db31846d3ba2c0e2a2b8f2
hzokbe <hzokbe@gmail.com>
2025-08-03 18:20:00 -0300

    refactor: remove unused imports

--- src/main/java/com/hzokbe/eigakan/controller/user/UserController.java
+++ src/main/java/com/hzokbe/eigakan/controller/user/UserController.java
@@ -1,13 +1,8 @@
 package com.hzokbe.eigakan.controller.user;
 
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.*;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
