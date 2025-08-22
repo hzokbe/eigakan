d25f5b68d674360154184b17e2efd05be5410a09
hzokbe <hzokbe@gmail.com>
2025-08-21 21:31:07 -0300

    test(movie): inject MovieService and MovieRepository

--- src/test/java/com/hzokbe/eigakan/service/movie/MovieServiceTest.java
+++ src/test/java/com/hzokbe/eigakan/service/movie/MovieServiceTest.java
@@ -1,8 +1,16 @@
 package com.hzokbe.eigakan.service.movie;
 
+import com.hzokbe.eigakan.repository.movie.MovieRepository;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class MovieServiceTest {
+    @InjectMocks
+    private MovieService service;
+
+    @Mock
+    private MovieRepository repository;
 }
