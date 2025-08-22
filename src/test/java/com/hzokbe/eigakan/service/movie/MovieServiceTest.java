02e642293201a13827d52a001c2360d0fb9d54a4
hzokbe <hzokbe@gmail.com>
2025-08-21 21:30:04 -0300

    test(movie): add MovieServiceTest class

---
+++ src/test/java/com/hzokbe/eigakan/service/movie/MovieServiceTest.java
@@ -0,0 +1,8 @@
+package com.hzokbe.eigakan.service.movie;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class MovieServiceTest {
+}
