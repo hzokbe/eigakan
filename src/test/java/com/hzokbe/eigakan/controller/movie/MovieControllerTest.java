03aeae91ba0bc90ccdf53ca3a27be5095c19a037
hzokbe <hzokbe@gmail.com>
2025-08-24 13:17:07 -0300

    test(movie): add MovieControllerTest class

---
+++ src/test/java/com/hzokbe/eigakan/controller/movie/MovieControllerTest.java
@@ -0,0 +1,7 @@
+package com.hzokbe.eigakan.controller.movie;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(MovieController.class)
+class MovieControllerTest {
+}
