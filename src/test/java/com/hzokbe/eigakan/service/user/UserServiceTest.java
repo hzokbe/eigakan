12210b9611f4fb232d4287c78063576fdae795b9
hzokbe <hzokbe@gmail.com>
2025-08-24 16:01:57 -0300

    test(user): add save user with invalid raw password test

--- src/test/java/com/hzokbe/eigakan/service/user/UserServiceTest.java
+++ src/test/java/com/hzokbe/eigakan/service/user/UserServiceTest.java
@@ -1,84 +1,99 @@
-package com.hzokbe.eigakan.service.user;
+    package com.hzokbe.eigakan.service.user;
 
-import com.hzokbe.eigakan.exception.user.AlreadyRegisteredUserException;
-import com.hzokbe.eigakan.exception.user.InvalidPasswordException;
-import com.hzokbe.eigakan.exception.user.InvalidUsernameException;
-import com.hzokbe.eigakan.model.user.request.UserRequest;
-import com.hzokbe.eigakan.repository.user.UserRepository;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+    import com.hzokbe.eigakan.exception.user.AlreadyRegisteredUserException;
+    import com.hzokbe.eigakan.exception.user.InvalidPasswordException;
+    import com.hzokbe.eigakan.exception.user.InvalidUsernameException;
+    import com.hzokbe.eigakan.model.user.request.UserRequest;
+    import com.hzokbe.eigakan.repository.user.UserRepository;
+    import org.junit.jupiter.api.Test;
+    import org.junit.jupiter.api.extension.ExtendWith;
+    import org.mockito.InjectMocks;
+    import org.mockito.Mock;
+    import org.mockito.junit.jupiter.MockitoExtension;
+    import org.springframework.security.crypto.password.PasswordEncoder;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
+    import static org.junit.jupiter.api.Assertions.assertThrows;
+    import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
-class UserServiceTest {
-    @InjectMocks
-    private UserService service;
+    @ExtendWith(MockitoExtension.class)
+    class UserServiceTest {
+        @InjectMocks
+        private UserService service;
 
-    @Mock
-    private UserRepository repository;
+        @Mock
+        private UserRepository repository;
 
-    @Test
-    public void shouldThrowInvalidUsernameException_whenSavingUserWithNullUsername() {
-        var request = new UserRequest(null, "bar");
+        @Mock
+        private PasswordEncoder encoder;
 
-        assertThrows(InvalidUsernameException.class, () -> service.signUp(request));
+        @Test
+        public void shouldThrowInvalidUsernameException_whenSavingUserWithNullUsername() {
+            var request = new UserRequest(null, "bar");
+
+            assertThrows(InvalidUsernameException.class, () -> service.signUp(request));
+        }
+
+        @Test
+        public void shouldThrowInvalidUsernameException_whenSavingUserWithBlankUsername() {
+            var request = new UserRequest("", "bar");
+
+            assertThrows(InvalidUsernameException.class, () -> service.signUp(request));
+
+            request.setUsername(" ");
+
+            assertThrows(InvalidUsernameException.class, () -> service.signUp(request));
+        }
+
+        @Test
+        public void shouldThrowInvalidUsernameException_whenSavingUserWithInvalidUsername() {
+            var request = new UserRequest("foo!", "bar");
+
+            assertThrows(InvalidUsernameException.class, () -> service.signUp(request));
+
+            request.setUsername("foo@");
+
+            assertThrows(InvalidUsernameException.class, () -> service.signUp(request));
+
+            request.setUsername("foo#");
+
+            assertThrows(InvalidUsernameException.class, () -> service.signUp(request));
+        }
+
+        @Test
+        public void shouldThrowAlreadyRegisteredException_whenSavingAlreadyRegisteredUser() {
+            var request = new UserRequest("foo", "bar");
+
+            when(repository.existsByUsername("foo")).thenReturn(true);
+
+            assertThrows(AlreadyRegisteredUserException.class, () -> service.signUp(request));
+        }
+
+        @Test
+        public void shouldThrowInvalidPasswordException_whenSavingUserWithNullRawPassword() {
+            var request = new UserRequest("foo", null);
+
+            assertThrows(InvalidPasswordException.class, () -> service.signUp(request));
+        }
+
+        @Test
+        public void shouldThrowInvalidPasswordException_whenSavingUserWithBlankRawPassword() {
+            var request = new UserRequest("foo", "");
+
+            assertThrows(InvalidPasswordException.class, () -> service.signUp(request));
+
+            request.setRawPassword(" ");
+
+            assertThrows(InvalidPasswordException.class, () -> service.signUp(request));
+        }
+
+        @Test
+        public void shouldThrowInvalidPasswordException_whenSavingUserWithInvalidRawPassword() {
+            var request = new UserRequest("foo", "bar");
+
+            assertThrows(InvalidPasswordException.class, () -> service.signUp(request));
+
+            request.setRawPassword("foobar");
+
+            assertThrows(InvalidPasswordException.class, () -> service.signUp(request));
+        }
     }
-
-    @Test
-    public void shouldThrowInvalidUsernameException_whenSavingUserWithBlankUsername() {
-        var request = new UserRequest("", "bar");
-
-        assertThrows(InvalidUsernameException.class, () -> service.signUp(request));
-
-        request.setUsername(" ");
-
-        assertThrows(InvalidUsernameException.class, () -> service.signUp(request));
-    }
-
-    @Test
-    public void shouldThrowInvalidUsernameException_whenSavingUserWithInvalidUsername() {
-        var request = new UserRequest("foo!", "bar");
-
-        assertThrows(InvalidUsernameException.class, () -> service.signUp(request));
-
-        request.setUsername("foo@");
-
-        assertThrows(InvalidUsernameException.class, () -> service.signUp(request));
-
-        request.setUsername("foo#");
-
-        assertThrows(InvalidUsernameException.class, () -> service.signUp(request));
-    }
-
-    @Test
-    public void shouldThrowAlreadyRegisteredException_whenSavingAlreadyRegisteredUser() {
-        var request = new UserRequest("foo", "bar");
-
-        when(repository.existsByUsername("foo")).thenReturn(true);
-
-        assertThrows(AlreadyRegisteredUserException.class, () -> service.signUp(request));
-    }
-
-    @Test
-    public void shouldThrowInvalidPasswordException_whenSavingUserWithNullRawPassword() {
-        var request = new UserRequest("foo", null);
-
-        assertThrows(InvalidPasswordException.class, () -> service.signUp(request));
-    }
-
-    @Test
-    public void shouldThrowInvalidPasswordException_whenSavingUserWithBlankRawPassword() {
-        var request = new UserRequest("foo", "");
-
-        assertThrows(InvalidPasswordException.class, () -> service.signUp(request));
-
-        request.setRawPassword(" ");
-
-        assertThrows(InvalidPasswordException.class, () -> service.signUp(request));
-    }
-}
