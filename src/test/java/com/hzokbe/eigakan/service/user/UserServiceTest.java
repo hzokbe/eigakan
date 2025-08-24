026dd31bf1c7113e7763409ce93c460705f2be82
hzokbe <hzokbe@gmail.com>
2025-08-24 14:22:22 -0300

    test(user): inject UserService and UserRepository

--- src/test/java/com/hzokbe/eigakan/service/user/UserServiceTest.java
+++ src/test/java/com/hzokbe/eigakan/service/user/UserServiceTest.java
@@ -1,8 +1,16 @@
 package com.hzokbe.eigakan.service.user;
 
+import com.hzokbe.eigakan.repository.user.UserRepository;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
+    @InjectMocks
+    private UserService service;
+
+    @Mock
+    private UserRepository repository;
 }
