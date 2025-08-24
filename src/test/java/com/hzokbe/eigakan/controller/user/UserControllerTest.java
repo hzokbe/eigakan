a3157bb38c5cdff106ee21b56196916a2c5b994a
hzokbe <hzokbe@gmail.com>
2025-08-24 14:18:59 -0300

    test(user): add UserControllerTest class

---
+++ src/test/java/com/hzokbe/eigakan/controller/user/UserControllerTest.java
@@ -0,0 +1,7 @@
+package com.hzokbe.eigakan.controller.user;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+}
