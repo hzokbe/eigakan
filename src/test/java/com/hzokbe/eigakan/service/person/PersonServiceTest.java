058d8664dfda08fd863ed87d71fe03ad2abcc47a
hzokbe <hzokbe@gmail.com>
2025-08-24 10:07:45 -0300

    test(person): add save person with null name test

--- src/test/java/com/hzokbe/eigakan/service/person/PersonServiceTest.java
+++ src/test/java/com/hzokbe/eigakan/service/person/PersonServiceTest.java
@@ -1,10 +1,15 @@
 package com.hzokbe.eigakan.service.person;
 
+import com.hzokbe.eigakan.exception.person.InvalidPersonNameException;
+import com.hzokbe.eigakan.model.person.request.PersonRequest;
 import com.hzokbe.eigakan.repository.person.PersonRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
 class PersonServiceTest {
@@ -13,4 +18,11 @@ class PersonServiceTest {
 
     @Mock
     private PersonRepository repository;
+
+    @Test
+    public void shouldThrowInvalidPersonNameException_whenSavingPersonWithNullName() {
+        var request = new PersonRequest(null, "bar");
+
+        assertThrows(InvalidPersonNameException.class, () -> service.save(request));
+    }
 }
