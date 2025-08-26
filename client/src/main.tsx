8e5d761f4827992ca16d570733936e8491e3f752
hzokbe <hzokbe@gmail.com>
2025-08-26 19:41:34 -0300

    feat(client): add / route

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,14 @@ import "./index.css";
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 );
