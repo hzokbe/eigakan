fec2ebb9b0977161e5e815e1e6bf8154a508ba98
hzokbe <hzokbe@gmail.com>
2025-08-26 19:43:30 -0300

    feat(client): add /sign-up route and sign up page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
+import SignUpPage from "./sign-up/SignUpPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+
+        <Route path="/sign-up" element={<SignUpPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
