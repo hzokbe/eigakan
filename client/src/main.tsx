5611bdc9d9c6598d63b0336f7c4a88adbb3f028e
hzokbe <hzokbe@gmail.com>
2025-08-30 17:35:20 -0300

    feat(client): add /movies route and movies page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import SignUpPage from "./pages/sign-up/SignUpPage.tsx";
 import SignInPage from "./pages/sign-in/SignInPage.tsx";
 import ProfilePage from "./pages/profile/ProfilePage.tsx";
+import MoviesPage from "./pages/movies/MoviesPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -18,6 +19,8 @@ createRoot(document.getElementById("root")!).render(
         <Route path="/sign-in" element={<SignInPage />} />
 
         <Route path="/profile" element={<ProfilePage />} />
+
+        <Route path="/movies" element={<MoviesPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
