949695403989c45013c220e7b0d21a2b1d48e4a0
hzokbe <hzokbe@gmail.com>
2025-09-01 16:12:54 -0300

    feat(server): add global exception handler logger

--- server/src/main/java/com/hzokbe/eigakan/exception/GlobalExceptionHandler.java
+++ server/src/main/java/com/hzokbe/eigakan/exception/GlobalExceptionHandler.java
@@ -9,6 +9,8 @@ import com.hzokbe.eigakan.exception.user.InvalidEmailException;
 import com.hzokbe.eigakan.exception.user.InvalidPasswordException;
 import com.hzokbe.eigakan.exception.user.InvalidUsernameException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,6 +25,8 @@ import com.hzokbe.eigakan.model.exception.response.ExceptionResponse;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ResponseBody
@@ -34,6 +38,8 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
     public ExceptionResponse invalidMovieTitleExceptionHandler(InvalidMovieTitleException exception) {
+        logger.error("unhandled exception occurred", exception);
+
         return new ExceptionResponse(exception.getMessage());
     }
 
