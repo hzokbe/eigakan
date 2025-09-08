1b5beb00983d1ef067a3a0d147bf8c89664f875f
hzokbe <hzokbe@gmail.com>
2025-09-07 21:28:18 -0300

    refactor: remove unused import

--- server/src/test/java/com/hzokbe/eigakan/service/movie/MovieServiceTest.java
+++ server/src/test/java/com/hzokbe/eigakan/service/movie/MovieServiceTest.java
@@ -12,7 +12,6 @@ import com.hzokbe.eigakan.repository.movie.MovieRepository;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.Test;
