a9e79cf44e6ad7303a5411d8208b661b3c4d244f
hzokbe <hzokbe@gmail.com>
2025-09-08 21:03:39 -0300

    test(user): add user not found exception test

--- server/src/test/java/com/hzokbe/eigakan/controller/user/UserControllerTest.java
+++ server/src/test/java/com/hzokbe/eigakan/controller/user/UserControllerTest.java
@@ -1,12 +1,17 @@
 package com.hzokbe.eigakan.controller.user;
 
+import com.hzokbe.eigakan.exception.user.UserNotFoundException;
+import com.hzokbe.eigakan.service.user.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -15,10 +20,23 @@ class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockitoBean
+    private UserService service;
+
     @Test
     void shouldNotAuthorize_whenRequestWithoutAuthorizationHeader() throws Exception {
         var id = UUID.randomUUID().toString();
 
         mockMvc.perform(get("/users/" + id)).andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(username = "hzokbe")
+    void shouldReturnUserNotFound_whenRequestWithAuthorizationHeader() throws Exception {
+        var id = UUID.randomUUID().toString();
+
+        doThrow(UserNotFoundException.class).when(service).findById(id);
+
+        mockMvc.perform(get("/users/" + id)).andExpect(status().isBadRequest());
     }
 }
