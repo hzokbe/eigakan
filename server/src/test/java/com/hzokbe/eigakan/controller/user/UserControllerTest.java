29c265bf1cebc126bb62e40814269f8edd1f6199
hzokbe <hzokbe@gmail.com>
2025-09-08 21:00:33 -0300

    test(user): add check authorization test

--- server/src/test/java/com/hzokbe/eigakan/controller/user/UserControllerTest.java
+++ server/src/test/java/com/hzokbe/eigakan/controller/user/UserControllerTest.java
@@ -1,7 +1,24 @@
 package com.hzokbe.eigakan.controller.user;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
 class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldNotAuthorize_whenRequestWithoutAuthorizationHeader() throws Exception {
+        var id = UUID.randomUUID().toString();
+
+        mockMvc.perform(get("/users/" + id)).andExpect(status().isUnauthorized());
+    }
 }
