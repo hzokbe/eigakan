b91475780ea1ddf388623e11a5e97ecf7f033b98
hzokbe <hzokbe@gmail.com>
2025-09-10 19:33:02 -0300

    test(person): add throw already registered person exception when saving person already registered test

--- server/src/test/java/com/hzokbe/eigakan/service/person/PersonServiceTest.java
+++ server/src/test/java/com/hzokbe/eigakan/service/person/PersonServiceTest.java
@@ -1,5 +1,6 @@
 package com.hzokbe.eigakan.service.person;
 
+import com.hzokbe.eigakan.exception.person.AlreadyRegisteredPersonException;
 import com.hzokbe.eigakan.exception.person.InvalidPersonLastNameException;
 import com.hzokbe.eigakan.exception.person.InvalidPersonNameException;
 import com.hzokbe.eigakan.model.person.request.PersonRequest;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.doReturn;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -50,5 +52,14 @@ class PersonServiceTest {
         var request = new PersonRequest("foo", "");
 
         assertThrows(InvalidPersonLastNameException.class, () -> service.save(request));
+    }
+
+    @Test
+    public void shouldThrowAlreadyRegisteredPersonException_whenSavingPersonAlreadyRegistered() {
+        var request = new PersonRequest("foo", "bar");
+
+        doReturn(true).when(repository).existsByNameAndLastName("foo", "bar");
+
+        assertThrows(AlreadyRegisteredPersonException.class, () -> service.save(request));
     }
 }
