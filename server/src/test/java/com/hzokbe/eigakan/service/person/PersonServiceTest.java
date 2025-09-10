9d81abfdda0daa58a3212d5c8eb9d4723b95e01f
hzokbe <hzokbe@gmail.com>
2025-09-10 20:32:13 -0300

    refactor: remove unused import

--- server/src/test/java/com/hzokbe/eigakan/service/person/PersonServiceTest.java
+++ server/src/test/java/com/hzokbe/eigakan/service/person/PersonServiceTest.java
@@ -16,8 +16,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.UUID;
-
 @ExtendWith(MockitoExtension.class)
 class PersonServiceTest {
     @InjectMocks
