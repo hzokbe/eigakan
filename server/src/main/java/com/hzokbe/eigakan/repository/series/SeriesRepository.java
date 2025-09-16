5ec70908ab0dd8223d1346fbe7fbf974655de1a3
hzokbe <hzokbe@gmail.com>
2025-09-16 19:21:46 -0300

    feat(repository): add SeriesRepository interface

---
+++ server/src/main/java/com/hzokbe/eigakan/repository/series/SeriesRepository.java
@@ -0,0 +1,9 @@
+package com.hzokbe.eigakan.repository.series;
+
+import com.hzokbe.eigakan.model.series.Series;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SeriesRepository extends MongoRepository<Series, String> {
+}
