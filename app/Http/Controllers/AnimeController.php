798fbd6526772d1b53937cc08a94112eec91fa75
hzokbe <hzokbe@gmail.com>
2026-09-25 22:21:00 -0300

    animes: add filter to sort animes

--- app/Http/Controllers/AnimeController.php
+++ app/Http/Controllers/AnimeController.php
@@ -3,12 +3,33 @@
 namespace App\Http\Controllers;
 
 use App\Models\Anime;
+use Illuminate\Http\Request;
 
 class AnimeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $animes = Anime::all();
+        $allowedSorts = [
+            'title',
+            'score',
+            'aired_from'
+        ];
+
+        $sort = $request->input('sort', 'score');
+
+        if (!in_array($sort, $allowedSorts)) {
+            $sort = 'score';
+        }
+
+        $direction = $request->input('direction', 'desc');
+
+        if (!in_array($direction, ['asc', 'desc'])) {
+            $direction = 'desc';
+        }
+
+        $animes = Anime::query()
+            ->orderBy($sort, $direction)
+            ->get();
 
         return view('anime.index')
             ->with('animes', $animes);
